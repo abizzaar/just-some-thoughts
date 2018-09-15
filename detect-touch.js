5864c47471a012110b8bbf8177d010fcbc3dd96a
Abizar Bagasrawala <babyzar97@gmail.com>
2018-09-15 15:30:33 -0700

    optimized for diff screens

---
+++ detect-touch.js
@@ -0,0 +1,22 @@
+function is_touch_device() {
+  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+  var mq = function(query) {
+    return window.matchMedia(query).matches;
+  }
+
+  if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+    return true;
+  }
+
+  // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+  // https://git.io/vznFH
+  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+  return mq(query);
+}
+
+if (is_touch_device()) {
+  document.documentElement.className += " is-touch-device"
+}
+else {
+  document.documentElement.className += " is-not-touch-device"
+}
